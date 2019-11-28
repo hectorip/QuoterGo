b64f4e2eba8de47bb28cbc36cfa29c17f8c88034
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2019-11-27 23:59:06 -0600

    50 quotes

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
@@ -15,4 +19,38 @@ func helloHandler(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": fmt.Sprintf("Hola %s", c.Param("name")),
 	})
+}
+
+func randomQuote(c *gin.Context) {
+	quotes := getQuotes
+}
+
+type Quote struct {
+	Text   string
+	Author string
+}
+
+func getQuotes() (quotes []Quote) {
+	// Open the file
+	csvfile, err := os.Open("spanish.csv")
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+
+	// Parse the file
+	r := csv.NewReader(csvfile)
+	//r := csv.NewReader(bufio.NewReader(csvfile))
+
+	// Iterate through the records
+	for {
+		// Read each record from csv
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Quote: %s By %s\n", record[0], record[1])
+	}
 }
